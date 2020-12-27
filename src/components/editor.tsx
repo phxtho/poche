ffb2462c23a4a4fb34b7ff89585d7ceb638ed0b5
phxtho <phathopukwana@gmail.com>
2020-12-27 18:30:37 +0200

    useRef instead of id

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {EditorState} from 'prosemirror-state'
 import {EditorView} from 'prosemirror-view'
 import {schema}  from "../model/schema"
@@ -6,17 +6,18 @@ import {exampleSetup} from "prosemirror-example-setup"
 import './editor.css'
 
 const Editor = () => {
+  const editorRef = useRef()
   const [editorView, setEditorView] = useState(null);
 
   const editorState = EditorState.create({schema: schema, plugins: exampleSetup({schema: schema})})
   
   useEffect(()=>{
-    let newEditorView = new EditorView(document.querySelector("#editor"), {state: editorState})
+    let newEditorView = new EditorView(editorRef.current, {state: editorState})
     setEditorView(newEditorView)
   },[])
 
  return <div id="editorContainer">
-   <div id="editor"></div>
+   <div id="editor" ref={editorRef}></div>
  </div>
 }
 
