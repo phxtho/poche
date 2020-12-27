4461f717b841e6cc1e3d74e234e3b6e0b7b5b454
phxtho <phathopukwana@gmail.com>
2020-12-27 16:41:11 +0200

    add prosemirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'remirror/styles/all.css';
 import EditorWrapper from "./components/editor";
 
 function App() {
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
+import {EditorState} from 'prosemirror-state'
+import {EditorView} from 'prosemirror-view'
+import {schema}  from "../model/schema"
 
-const editor = () => {
- return <div></div>
+const Editor = () => {
+  const editorRef = useRef(null);
+  const editorState = EditorState.create({schema: schema})
+  const prosemirrorView = new EditorView(editorRef.current || undefined, {state: editorState})
+
+ return <div id="editor" ref={editorRef}></div>
 }
 
-export default editor
+export default Editor
