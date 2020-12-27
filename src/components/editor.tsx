42f98aa130fd0b48a73f5580c079327ee749d0ba
phxtho <phathopukwana@gmail.com>
2020-12-27 14:06:26 +0200

    remove remirror

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,61 +1,7 @@
-import React from 'react';
-import { BoldExtension } from 'remirror/extension/bold';
-import { ItalicExtension } from 'remirror/extension/italic';
-import { UnderlineExtension } from 'remirror/extension/underline';
-import { CorePreset } from 'remirror/preset/core';
-import { RemirrorProvider, useManager, useRemirror } from 'remirror/react';
+import React from "react";
 
+const editor = () => {
+ return <div></div>
+}
 
-const EditorWrapper = () => {
-  const manager = useManager([
-    new CorePreset({}),
-    new BoldExtension({}),
-    new ItalicExtension(),
-    new UnderlineExtension(),
-  ]);
-
-  return (
-    <RemirrorProvider manager={manager}>
-      <div>
-        <Menu />
-        <TextEditor />
-      </div>
-    </RemirrorProvider>
-  );
-};
-
-const Menu = () => {
-    const { commands, active } = useRemirror({ autoUpdate: true });
-  
-    return (
-      <div>
-        <button
-          onClick={() => commands.toggleBold()}
-          style={{ fontWeight: active.bold() ? 'bold' : undefined }}
-        >
-          B
-        </button>
-        <button
-          onClick={() => commands.toggleItalic()}
-          style={{ fontWeight: active.italic() ? 'bold' : undefined }}
-        >
-          I
-        </button>
-        <button
-          onClick={() => commands.toggleUnderline()}
-          style={{ fontWeight: active.underline() ? 'bold' : undefined }}
-        >
-          U
-        </button>
-      </div>
-    );
-  };
-  
-  const TextEditor = () => {
-    const { getRootProps } = useRemirror();
-  
-    return <div {...getRootProps()} />;
-  };
-
-
-export default EditorWrapper;
+export default editor
