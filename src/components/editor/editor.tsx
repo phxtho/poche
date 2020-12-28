7d7ca82da14ce3feb7f8bd35b8be46a0ee7b8e4f
phxtho <phathopukwana@gmail.com>
2020-12-29 00:47:12 +0200

    editor onFocus & onBlur event

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,18 +1,50 @@
 import React, { useEffect, useRef, useState } from "react";
 import {EditorState} from 'prosemirror-state'
-import {EditorView} from 'prosemirror-view'
+import {DirectEditorProps, EditorView} from 'prosemirror-view'
 import {schema}  from "../../model/schema"
 import {exampleSetup} from "prosemirror-example-setup"
 import './editor.css'
 
-const Editor = () => {
-  const editorRef = useRef()
-  const [, setEditorView] = useState(null);
+interface EditorProps {
+  onFocus?,
+  onBlur?
+}
 
-  const editorState = EditorState.create({schema: schema, plugins: exampleSetup({schema: schema, menuBar: false})})
+const Editor = (props: EditorProps) => {
+  const createPlugins = () => {
+    try {
+      return exampleSetup({schema: schema, menuBar: false}) 
+    } catch (error) {
+      console.warn(`couldn't create editor plugins: ${error}`)
+    }
+  }
+
+  const createEditorViewProps = (editorState: EditorState):DirectEditorProps => {
+    const editorProps: DirectEditorProps = {state: editorState, handleDOMEvents: {
+      focus: (view: EditorView, event: FocusEvent) => {
+        const transaction = view.state.tr.setMeta('focused', true)
+        view.dispatch(transaction)
+        props.onFocus && props.onFocus();
+        return false
+      },
+      blur: (view: EditorView, event: FocusEvent) => {
+        const transaction = view.state.tr.setMeta('focused', false)
+        view.dispatch(transaction)
+        props.onBlur && props.onBlur()
+        return false
+      }}}
+    
+      return editorProps
+  }
+
+  
+  const editorRef = useRef()
+  const [, setEditorView] = useState<EditorView>(null);
   
   useEffect(()=>{
-    const editorView = new EditorView(editorRef.current, {state: editorState})
+    // Initialise the editor view
+    const editorState = EditorState.create({schema: schema, plugins: createPlugins()})
+    const editorView = new EditorView(editorRef.current, createEditorViewProps(editorState))
     setEditorView(editorView)
   },[])
 
