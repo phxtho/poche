2625a7ea20fb6420040f255c1ae9004f67c113f3
phxtho <phathopukwana@gmail.com>
2021-02-18 22:55:33 +0200

    immutability helper + redux = faster updates

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore, compose } from "redux";
 import { Canvas } from "model/interfaces";
+import update from "immutability-helper";
 
 export const ADD_ITEM_TO_CANVAS = "ADD_ITEM_TO_CANVAS";
 export const REMOVE_ITEM_FROM_CANVAS = "REMOVE_ITEM_FROM_CANVAS";
@@ -11,35 +12,37 @@ const openCanvas: Canvas = {
   items: [],
 };
 
-const reducer = function (state = { openCanvas: openCanvas }, action) {
-  switch (action.type) {
+const reducer = function (
+  state = { openCanvas: openCanvas },
+  { type, payload }
+) {
+  switch (type) {
     case ADD_ITEM_TO_CANVAS:
       const { items } = state.openCanvas;
-      if (!items.find((item) => item.id === action.payload?.id)) {
-        const updatedItems = [...items, action.payload];
-        const canvas = { ...state.openCanvas, items: updatedItems };
-        return { ...state, openCanvas: canvas };
+      if (!items.find((item) => item.id === payload?.id)) {
+        state.openCanvas.items = update(items, { $push: [payload] });
+        return state;
       }
       return state;
     case REMOVE_ITEM_FROM_CANVAS:
       state.openCanvas.items = state.openCanvas.items.filter(
-        (item) => item.id !== action.payload?.id
+        (item) => item.id !== payload?.id
       );
       return state;
     case UPDATE_ITEM:
       const oldItem = state.openCanvas.items.find(
-        (item) => item.id === action.payload?.id
+        (item) => item.id === payload?.id
       );
 
       if (!oldItem) {
         return state;
       }
 
-      const newItem = { ...oldItem, ...action.payload };
-      state.openCanvas.items = state.openCanvas.items.filter(
-        (item) => item.id !== action.payload?.id
-      );
-      state.openCanvas.items.push(newItem);
+      const itemIdx = state.openCanvas.items.indexOf(oldItem);
+      state.openCanvas.items = update(state.openCanvas.items, {
+        [itemIdx]: { $merge: { ...payload } },
+      });
+
       return state;
 
     default:
