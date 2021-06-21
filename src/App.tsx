0492f1f013f2dc275fa38a38260b2ceadeca68c1
phxtho <phathopukwana@gmail.com>
2021-06-21 22:37:49 +0200

    remove outline

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Routes from "router/Routes";
 import { Provider } from "react-redux";
 import store from "store";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
