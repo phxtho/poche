21fc484c97661c94e22559fb87f1b8a80abe703d
phxtho <phathopukwana@gmail.com>
2021-09-22 19:59:53 +0200

    add library page

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Editor from "components/remirror-editor/remirror-editor";
+import SearchBar from "components/searchbar/searchbar";
 
 export default function Demo() {
   return (
     <div className="min-h-screen">
-      <Editor />
+      <div id="top-bar" className="flex flex-row-reverse p-8">
+        <SearchBar />
+      </div>
     </div>
   );
 }
---
+++ src/pages/library.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { getNotes, onChange } from "db/pouch/notes";
+import { ADD_ITEM_TO_CANVAS } from "store";
+import { useDispatch, useSelector } from "react-redux";
+import { ICanvas, ICanvasCard, INote, ItemTypes } from "model/interfaces";
+import { useNavigate } from "@reach/router";
+
+export default function Library() {
+  const [allNotes, setAllNotes] = useState([]);
+
+  const openCanvasCards: ICanvasCard[] = useSelector(
+    (state: { openCanvas: ICanvas }) => state.openCanvas.items
+  );
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const addNoteToCanvas = (note: INote) => {
+    const item: ICanvasCard = {
+      id: note?.id,
+      type: ItemTypes.CARD,
+      x: 690,
+      y: 320,
+    };
+    dispatch({ type: ADD_ITEM_TO_CANVAS, payload: item });
+  };
+
+  const fetchAllNotes = useCallback(async () => {
+    const response = await getNotes();
+    setAllNotes(response);
+  }, [setAllNotes]);
+
+  useEffect(() => {
+    fetchAllNotes();
+    onChange(fetchAllNotes);
+  }, [fetchAllNotes]);
+
+  return (
+    <div className="min-h-screen">
+      <div className="h-full w-full rounded-lg divide-y divide-gray-200">
+        {allNotes.map((note, idx) => {
+          const noteText = note?.state?.doc ? note.text : "";
+
+          return (
+            <button
+              className="truncate w-full transition duration-250 ease-in-out bg-gradient-to-r hover:from-gray-100 hover:to-gray-200"
+              key={idx}
+              onClick={() => {
+                addNoteToCanvas(note);
+                navigate("/experiment-501.V2/p");
+              }}
+            >
+              {note.title || noteText}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
