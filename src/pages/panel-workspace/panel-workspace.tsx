b9962ed3d1ddbae03bad6942ac1b770d752ac760
phxtho <phathopukwana@gmail.com>
2021-10-15 11:58:45 +0200

    navigte back if there are no open cards

--- src/pages/panel-workspace/panel-workspace.tsx
+++ src/pages/panel-workspace/panel-workspace.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "components/searchbar/searchbar";
 import { useSelector } from "react-redux";
 import { ICanvas, ICanvasCard } from "model/interfaces";
 import EditorContainer from "components/editor-container/editor-container";
 import AddNoteFAB from "components/add-note-fab/add-note-fab";
 import "./panel-workspace.css";
+import { useNavigate } from "@reach/router";
 
 export default function PanelWorkspace() {
   const cards: ICanvasCard[] = useSelector(
     (state: { openCanvas: ICanvas }) => state.openCanvas.items
   );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cards.length == 0) {
+      navigate("/experiment-501.V2");
+    }
+  }, [cards, navigate]);
 
   return (
     <div className="min-h-screen">
