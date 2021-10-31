00b0d7129ce6457530677964ca7b103223846940
phxtho <phathopukwana@gmail.com>
2021-10-31 20:48:03 +0200

    add note fab to explicity insert note in db

--- src/components/add-note-fab/add-note-fab.tsx
+++ src/components/add-note-fab/add-note-fab.tsx
@@ -1,17 +1,18 @@
 import React, { useCallback } from "react";
-import { ItemTypes, ICanvasCard, INote, ICanvas } from "model/interfaces";
+import { ItemTypes, ICanvasCard } from "model/interfaces";
 import { VscAdd } from "react-icons/vsc";
 import { useDispatch } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
 import { ADD_ITEM_TO_CANVAS } from "store";
+import { insertNote } from "db/pouch/notes";
 
 export default function AddNoteFAB(props) {
   const dispatch = useDispatch();
 
-  const createCard = useCallback(() => {
+  const createCard = useCallback(async () => {
+    let newNote = await insertNote();
     const newCard: ICanvasCard = {
       type: ItemTypes.CARD,
-      id: uuidv4(),
+      id: newNote.id,
       x: 0,
       y: 0,
     };
@@ -24,7 +25,7 @@ export default function AddNoteFAB(props) {
         createCard();
         props?.handleClick?.();
       }}
-      className="rounded-full h-16 w-16 fixed right-8 bottom-8 bg-black text-white flex justify-center items-center shadow-md hover:pointer"
+      className="rounded-full h-16 w-16 fixed right-8 bottom-8 bg-black text-white flex justify-center items-center shadow-md hover:pointer border-2 border-white"
     >
       <VscAdd />
     </button>
