8369b18440a55462fa6e17f59b7ead3011d963f4
phxtho <phathopukwana@gmail.com>
2021-10-31 20:47:00 +0200

    simplify editor container by removing reducer

--- src/components/editor-container/editor-container.tsx
+++ src/components/editor-container/editor-container.tsx
@@ -1,7 +1,7 @@
 /* Manage the editor's interaction with the db & global state */
-import React, { useCallback, useEffect, useReducer, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import Editor from "components/remirror-editor/remirror-editor";
-import { insertNote, updateNote, getNoteById } from "db/pouch/notes";
+import { updateNote, getNoteById } from "db/pouch/notes";
 import { INote } from "model/interfaces";
 import { useState } from "react";
 import { ReactFrameworkOutput } from "@remirror/react";
@@ -9,57 +9,29 @@ import { Extension } from "@remirror/core";
 import NoteOptionsModal from "components/note-options-modal/note-options-modal";
 
 interface EditorContainerProps {
-  id?: string;
+  id: string;
   note?: INote;
+  handleFocus?;
 }
 
-const SET_NOTE = "SET_NOTE";
-const SET_NOTE_STATE = "SET_NOTE_STATE";
-const NEW_NOTE = "NEW_NOTE";
-
-const reducer = (note: INote, { type, payload }) => {
-  switch (type) {
-    case SET_NOTE:
-      return payload;
-    case NEW_NOTE:
-      let currentTime = Date.now();
-      const newNote = {
-        ...payload,
-        createdTime: currentTime,
-        lastEditedTime: currentTime,
-      };
-      return newNote;
-    case SET_NOTE_STATE:
-      const updatedNote = {
-        ...note,
-        state: payload,
-        lastEditedTime: Date.now(),
-      };
-      return updatedNote;
-  }
-};
 const EditorContainer = (props: EditorContainerProps) => {
-  const [note, dispatch] = useReducer(reducer, props.note);
+  // const [note, dispatch] = useReducer(reducer, props.note);
+  const [note, setNote] = useState<INote>(props.note);
   const [focused, setFocused] = useState(false);
   const [noteOptionsOpen, setNoteOptionsOpen] = useState<boolean>(false);
 
   const ctxRef = useRef<ReactFrameworkOutput<Extension>>();
 
   const initialiseNote = useCallback(async () => {
-    if (props.id) {
+    if (props.id && !props.note) {
       // try fetch the note from the db
       let doc = await getNoteById(props.id);
       if (doc) {
-        dispatch({ type: SET_NOTE, payload: doc });
-      } else {
-        // No note found create one with the passed in id
-        const doc = await insertNote(props.id);
-        dispatch({ type: NEW_NOTE, payload: doc });
+        setNote(doc as unknown as INote);
       }
-    } else {
-      // No id create note with db generated id
-      const doc = await insertNote();
-      dispatch({ type: NEW_NOTE, payload: doc });
+    } else if (props.note) {
+      // Use the passed in note
+      setNote(props.note);
     }
   }, [props.id]);
 
@@ -69,10 +41,12 @@ const EditorContainer = (props: EditorContainerProps) => {
   }, [initialiseNote]);
 
   const handleOnChange = useCallback((params) => {
-    dispatch({
-      type: SET_NOTE_STATE,
-      payload: params.state.toJSON(),
-    });
+    const updatedNote = {
+      ...note,
+      state: params.state.toJSON(),
+      lastEditedTime: Date.now(),
+    };
+    setNote(updatedNote);
   }, []);
 
   const handleBlur = useCallback(
@@ -86,6 +60,7 @@ const EditorContainer = (props: EditorContainerProps) => {
 
   const handleFocus = useCallback((params, e) => {
     setFocused(true);
+    props.handleFocus(ctxRef.current);
   }, []);
 
   if (!note) return null;
@@ -108,7 +83,7 @@ const EditorContainer = (props: EditorContainerProps) => {
           />
           <div className="flex space-x-2">
             <button
-              className="h-4 w-4 rounded-full bg-green-300 hover:bg-green-500"
+              className="h-4 w-4 rounded-full bg-black hover:bg-gray-700"
               onClick={() => setNoteOptionsOpen(true)}
             />
           </div>
