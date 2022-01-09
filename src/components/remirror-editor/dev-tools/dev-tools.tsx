cfb2a35366768235c7b175be9d7677b8f938b633
phxtho <phathopukwana@gmail.com>
2022-01-09 04:02:21 +0200

    add dev-tools

---
+++ src/components/remirror-editor/dev-tools/dev-tools.tsx
@@ -0,0 +1,29 @@
+import applyDevTools from "prosemirror-dev-tools";
+import React, { useEffect } from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { useRemirrorContext } from "@remirror/react";
+
+var ProsemirrorDevTools = () => {
+  var _useRemirrorContext = useRemirrorContext(),
+    view = _useRemirrorContext.view;
+
+  useEffect(() => {
+    applyDevTools(view);
+    return () => {
+      var node = document.querySelector(".__prosemirror-dev-tools__");
+
+      if (!node) {
+        return;
+      }
+
+      unmountComponentAtNode(node);
+
+      if (node.parentNode) {
+        node.remove();
+      }
+    };
+  }, [view]);
+  return null;
+};
+
+export default ProsemirrorDevTools;
