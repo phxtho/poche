bad99238337c3624ed54ba8946a786a8f671e96b
phxtho <phathopukwana@gmail.com>
2022-01-11 22:50:41 +0200

    delete augio import script

--- src/utils/import/augio.ts
+++
@@ -1,7 +0,0 @@
-import { insertNote } from "db/pouch/notes";
-
-export default function ImportFromAugio(augioJson: any[]) {
-  augioJson.forEach((x) => {
-    insertNote(x.id, x.title, null, x.document, x.creationTime, x.updateTime);
-  });
-}
