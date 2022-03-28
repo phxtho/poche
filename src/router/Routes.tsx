1ace4c6a223ad066cd0af941bacd045461e33976
phxtho <phathopukwana@gmail.com>
2022-03-28 23:11:29 +0200

    add route based code splitting

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,23 +1,26 @@
-import CanvasWorkspace from "@/pages/canvas-workspace";
+import { lazy, Suspense } from "react";
 import PanelWorkspace from "@/pages/panel-workspace/panel-workspace";
 import Library from "@/pages/library/library";
-import Demo from "@/pages/demo";
-import Settings from "@/pages/settings/settings";
 import AppShell from "@/components/shell/shell";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+const Settings = lazy(() => import("@/pages/settings/settings"));
+const CanvasWorkspace = lazy(() => import("@/pages/canvas-workspace"));
+const Demo = lazy(() => import("@/pages/demo"));
 
 export default function Router(props) {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="poche" element={<AppShell />}>
-          <Route index element={<Library />} />
-          <Route path="canvas" element={<CanvasWorkspace />} />
-          <Route path="panel" element={<PanelWorkspace />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="demo" element={<Demo />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="poche" element={<AppShell />}>
+            <Route index element={<Library />} />
+            <Route path="canvas" element={<CanvasWorkspace />} />
+            <Route path="panel" element={<PanelWorkspace />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="demo" element={<Demo />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
