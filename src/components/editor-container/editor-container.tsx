2b04344229c83dfea71fbc0f2825470a1a050e94
phxtho <phathopukwana@gmail.com>
2022-04-18 02:35:23 +0200

    mess with editor sizes

--- src/components/editor-container/editor-container.tsx
+++ src/components/editor-container/editor-container.tsx
@@ -86,7 +86,7 @@ const EditorContainer = (props: EditorContainerProps) => {
       <div
         id={props.id}
         ref={elRef}
-        className="w-full md:w-5/12 lg:w-1/3 hover:shadow-lg rounded-lg p-5 lg:mr-4 mb-4 editor-container"
+        className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 hover:shadow-lg rounded-lg p-5 lg:mr-4 mb-4 editor-container"
       >
         <div className="flex justify-between">
           <textarea
