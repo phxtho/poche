6630065f24112679cf9085989ab6f14853664815
phxtho <phathopukwana@gmail.com>
2022-10-17 23:23:07 +0200

    remove drag and drop canvas

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -3,16 +3,14 @@ import PanelWorkspace from "@/pages/panel-workspace/panel-workspace";
 import Library from "@/pages/library/library";
 import AppShell from "@/components/shell/shell";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-const CanvasWorkspace = lazy(() => import("@/pages/canvas-workspace"));
 
-export default function Router(props) {
+export default function Router() {
   return (
     <BrowserRouter basename="/poche">
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<AppShell />}>
             <Route index element={<Library />} />
-            <Route path="canvas" element={<CanvasWorkspace />} />
             <Route path="panel" element={<PanelWorkspace />} />
           </Route>
         </Routes>
